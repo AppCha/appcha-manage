56f7a2ff1eab1882393b80069768529e5feb10cf
Floyd Price <f.price@componentworkshop.com>
2015-07-23 12:28:57 +0100

    Removed boilerplate ui elements from nav, replaced with (placeholder) profile and signout link

--- modules/theme/client/nav/nav.js
+++ modules/theme/client/nav/nav.js
@@ -1,3 +1,9 @@
 Template.MainNav.onRendered(function(){
   $(".nav-dropdown-button").dropdown();
 });
+
+Template.MainNav.helpers({
+  profileName: function() {
+    return Meteor.user().emails[0].address;
+  }
+});
